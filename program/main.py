b799e9572000280977ef44599ca83e8e9a7f43df
Eamon O'Grady <110887130+eee12342@users.noreply.github.com>
2025-10-15 13:16:31 +1100

    Add login page and route with password hashing
    Implemented a login page with its template and CSS. Added a Flask route for login that hashes the password using SHA-256 before processing. Updated index.html to link the Sign In button to the login page and improved CSS for anchor tags.

--- program/main.py
+++ program/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+import hashlib
 
 app = Flask(__name__)
 
@@ -6,6 +7,24 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+# login page
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # this means we are trying to login
+    if request.method == "POST":
+        username = request.form["username"]
+
+        # hash the password BEFORE checking it against the database
+        sha256_hash = hashlib.sha256()
+        sha256_hash.update(request.form.get["password"].encode('utf-8'))
+        hashed_password = sha256_hash.hexdigest()
+        print(hashed_password)
+
+        return redirect(url_for("hello_world"))
+    
+    # if we are simply request the page, just load it
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
